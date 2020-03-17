e89341844162a259e5f861c9e6100eec1339bfe0
malngaawy <mohager477@gmail.com>
2020-03-18 00:44:36 +0200

    start app and add bootstrap and add form component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+// importing Components
+import Form  from "./Form.js"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <div className="container form-container">
+      </div>
+    </div>
+  );
+}
+
+export default App;
