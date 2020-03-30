b091de4091ad5f69c4b0955a4d45efaa664357de
malngaawy <mohager477@gmail.com>
2020-03-30 15:51:26 +0200

    Updates

---
+++ precache-manifest.40a7f278d675f9d3ee2d8c481a8c94df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52816b650d6049dc5e8d67ca27999b81",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "d98aba926f0f39e9d0a5",
+    "url": "/profile/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9afd41b43ad06b33e766",
+    "url": "/profile/static/css/main.05ba6bfd.chunk.css"
+  },
+  {
+    "revision": "d98aba926f0f39e9d0a5",
+    "url": "/profile/static/js/2.c8df5c41.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/profile/static/js/2.c8df5c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9afd41b43ad06b33e766",
+    "url": "/profile/static/js/main.3d2778b8.chunk.js"
+  },
+  {
+    "revision": "300f845b75429fcc7a15",
+    "url": "/profile/static/js/runtime-main.bf9a5bd9.js"
+  },
+  {
+    "revision": "d854840cf817113571b043bd6b357f2f",
+    "url": "/profile/static/media/avatar.d854840c.jpg"
+  }
+]);
