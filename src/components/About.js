232f58815078b7f708ea865304c9cb29db42ef4e
malngaawy <mohager477@gmail.com>
2020-03-30 19:26:29 +0200

    test skills

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import avatar from '../assets/avatar.jpg'
 
-
 export default function About() {
   return (
     <div className='right-side about'>
       <p><span>Hey</span> my name is Mohammed Abdulmpneim i'm a frontend developer with some backend knowledge</p>
+      <h5>My Skills</h5>
+      <ul>
+        <li>Html</li>
+        <li>Css</li>
+        <li>Sass</li>
+        <li>Bootstrap</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>React.js</li>
+      </ul>
       <div>
         <img  src={avatar} alt='avatar' />
       </div>
