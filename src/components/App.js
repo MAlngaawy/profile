d3604a9654cf60cbb5d935aa01869e7926bdbac8
malngaawy <mohager477@gmail.com>
2020-04-01 11:07:31 +0200

    ss

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ const SideBar = () =>{
         <img src={avatar} />
       </div>
       <ul>
-        <li> <NavLink exact to="/profile" activeClassName='active' > About </NavLink> </li>
+        <li> <NavLink exact to="/" activeClassName='active' > About </NavLink> </li>
         <li> <NavLink to="/projects" activeClassName='active' > Projects </NavLink> </li>
         <li> <NavLink to="/connect" activeClassName='active' > connect me </NavLink> </li>
       </ul>
@@ -34,7 +34,7 @@ export default function App() {
       <div className='App'>
         <SideBar />
         <Switch>
-          <Route exact path="/profile" component={About} />
+          <Route exact path="/" component={About} />
           <Route path="/projects" component={Projects} />
           <Route path="/connect" component={Connect} />
         </Switch>
